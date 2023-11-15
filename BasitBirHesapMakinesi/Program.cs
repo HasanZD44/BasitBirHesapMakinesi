a2e058007f77e77fb23fcb6973f552fee3d0cfd6
Casper <Casper@DESKTOP-IOSCEH9>
2023-11-15 21:22:16 +0300

    Add project files.

---
+++ BasitBirHesapMakinesi/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace BasitBirHesapMakinesi
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			
+
+			
+			Console.WriteLine("İşlem seçiniz : [ + | - | * | / ] Çıkış yapmak için boş geçiniz");
+
+			double firstNum, secondNum, result = 0;
+			string mathOp;
+
+
+
+			Console.WriteLine("Sayi 1 : ");
+			firstNum = Convert.ToDouble(Console.ReadLine());
+			Console.WriteLine("Sayi 2 : ");
+			secondNum = Convert.ToDouble(Console.ReadLine());
+
+			Console.WriteLine("işlem: ");
+			mathOp = Console.ReadLine();
+
+			if (mathOp != string.Empty)
+			{
+
+				switch (mathOp)
+				{
+					case "+":
+
+						result = firstNum + secondNum;
+						Console.WriteLine("toplam:" + result);
+						break;
+
+					case "-":
+
+						result = firstNum - secondNum;
+						Console.WriteLine("Fark: {0}", result);
+						break;
+
+					case "*":
+
+						result = firstNum * secondNum;
+						Console.WriteLine($"Çarpım: {result}");
+						break;
+
+					case "/":
+
+						result = firstNum / secondNum;
+						Console.WriteLine($"Bölüm : {result}");
+						break;
+					default:
+						break;
+				}
+			}
+			else
+			{
+				Console.WriteLine("İşlem yapılmadı..");
+
+			}
+		}
+	}
+}
